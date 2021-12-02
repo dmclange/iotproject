9a7ffa5372efb036f88252754db722c2d6ceead4
dmclange <74191497+dmclange@users.noreply.github.com>
2021-12-02 18:33:35 +0200

    finally
    took wayyy to long

--- ProposalForOffsiteDataRetention.sql
+++ ProposalForOffsiteDataRetention.sql
@@ -1,5 +1,57 @@
-CREATE DATABASE `smorts`;
-USE smorts;
-CREATE TABLE `lights` (`id` SMALLINT unsigned NOT NULL AUTO_INCREMENT,`name` TEXT,`status` BOOLEAN NOT NULL,`added` DATE,PRIMARY KEY (`id`));
-CREATE TABLE `shutter` (`id` SMALLINT unsigned NOT NULL AUTO_INCREMENT,`status` BOOLEAN,`added` DATE,`location` VARCHAR,`name` VARCHAR,PRIMARY KEY (`id`));
-CREATE TABLE `sensor` (`id` SMALLINT unsigned NOT NULL AUTO_INCREMENT,`level` SMALLINT unsigned,`lastchange` DATE,`added` DATE,`location` VARCHAR,PRIMARY KEY (`id`));
+CREATE TABLE `activations` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `profileid` SMALLINT,
+  `starttime` TIME,
+  `actuatorid` SMALLINT
+);
+
+CREATE TABLE `actuators` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR,
+  `roomid` SMALLINT,
+  `state` boolean,
+  `color` MEDIUMINT,
+  `intensity` SMALLINT
+);
+
+CREATE TABLE `profiles` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userid` SMALLINT,
+  `dayofweek` VARCHAR,
+  `lastmodified` TIMEDATE
+);
+
+CREATE TABLE `rooms` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userid` SMALLINT,
+  `name` VARCHAR
+);
+
+CREATE TABLE `sensors` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR,
+  `roomid` SMALLINT,
+  `lastpolled` TIMEDATE,
+  `added` TIMEDATE,
+  `temp` SMALLINT,
+  `lightlevel` SMALLINT
+);
+
+CREATE TABLE `users` (
+  `id` SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR,
+  `created` TIMEDATE,
+  `lastactive` TIMEDATE
+);
+
+ALTER TABLE `activations` ADD FOREIGN KEY (`profileid`) REFERENCES `profiles` (`id`);
+
+ALTER TABLE `activations` ADD FOREIGN KEY (`actuatorid`) REFERENCES `actuators` (`id`);
+
+ALTER TABLE `actuators` ADD FOREIGN KEY (`roomid`) REFERENCES `rooms` (`id`);
+
+ALTER TABLE `profiles` ADD FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+ALTER TABLE `rooms` ADD FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+ALTER TABLE `sensors` ADD FOREIGN KEY (`roomid`) REFERENCES `rooms` (`id`);
