2fdd36a35d02a5efd186c1d2c4502ebca06f079a
Daniel Lange <74191497+dmclange@users.noreply.github.com>
2021-12-16 20:51:09 +0200

    simplifying to test publishing

--- cloud/pub.py
+++ cloud/pub.py
@@ -10,11 +10,6 @@ __license__ = "MIT"
 import paho.mqtt.client as mqtt
 import mariadb, sys, re, queue
 
-# lets make some global variables
-prevStateAct = 0
-num = 0
-client = mqtt.Client()
-
 def on_connect(client, userdata, flags, rc):
     # subscribing to topic so we know when to check database for updoots
     client.subscribe("database/+/update")
@@ -26,14 +21,10 @@ def on_message(client, userdata, msg):
     current = q.get()
     id = re.findall(r"/\d/",current[0])
     id = id[0].replace('/','')
-    if ("actuator" in msg.topic == True):
-        publishActuator(id)
+    publishActuator(client,id)
 
 
-def publishActuator(iid):
-    global prevStateAct
-    global num
-    global client
+def publishActuator(client, iid):
     try:
         conn = mariadb.connect(
             user="app",
@@ -47,25 +38,20 @@ def publishActuator(iid):
         print(f"Error connecting to DB: {e}")
         sys.exit(1)
     cur = conn.cursor()
-    try:
-        num = cur.execute(
+    cur.execute(
             "SELECT id,state,color,intensity FROM actuators WHERE id=" + iid + ";"
             )
-        if (prevStateAct != cur.state):
-            for (id,state,color,intensity) in cur:
-                pubtop = "actuators/"+id+"/state"
-                client.publish(topic=pubtop,payload=state)
-                pubtop = "actuators/"+id+"/color"
-                client.publish(topic=pubtop,payload=color)
-                pubtop = "actuators/"+id+"/intensity"
-                client.publish(topic=pubtop,payload=intensity)
-                prevStateAct = cur.state
-    except mariadb.Error as e:
-        print(f"Error: {e}")
+    for (id,state,color,intensity) in cur:
+        pubtop = "actuators/"+id+"/state"
+        client.publish(topic=pubtop,payload=state)
+        pubtop = "actuators/"+id+"/color"
+        client.publish(topic=pubtop,payload=color)
+        pubtop = "actuators/"+id+"/intensity"
+        client.publish(topic=pubtop,payload=intensity)
     conn.close()
 
 def main():
-    global client
+    client = mqtt.Client()
     client.connect("localhost", 1883, 60)
     client.on_connect = on_connect
     client.on_message = on_message
